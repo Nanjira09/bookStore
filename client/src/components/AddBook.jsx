5d70723a6d8f6a3123e9a638ad768312e89705fb
Abdul <abdul.nanjira@gmail.com>
2022-11-03 15:27:04 +0300

    Added form field

--- client/src/components/AddBook.jsx
+++ client/src/components/AddBook.jsx
@@ -1,7 +1,29 @@
 function AddBook() {
   return (
-    <form className="w-3/4 mx-auto rounded p-2 bg-gradient-to-br from-[#3E392B] to-[#7F7079]">
+    <form className="w-3/4 mx-auto my-4 flex flex-col justify-center items-center rounded p-2 bg-gradient-to-br from-[#202221] to-[#283A34]">
       <h2 className="text-center">Add A Book</h2>
+      <input
+        type="text"
+        className="w-full outline-none border-b placeholder:text-gray-100 placeholder:font-thin bg-transparent my-3"
+        placeholder="Title..."
+      />
+      <input
+        type="text"
+        className="w-full outline-none border-b placeholder:text-gray-100 placeholder:font-thin bg-transparent my-3"
+        placeholder="Genre..."
+      />
+      <select className="bg-transparent outline-none border-b self-start my-2">
+        <option>Select The Author</option>
+        <option>Nanjira</option>
+        <option>Abdul</option>
+        <option>Gamal</option>
+      </select>
+      <button
+        type="button"
+        className="bg-gradient-to-r from-[#22211F] to-[#442E29] my-3 cursor-pointer py-1 px-4 self-start rounded-xl"
+      >
+        Add To Books
+      </button>
     </form>
   );
 }
--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -3,18 +3,22 @@ function Container() {
     {
       id: 1,
       name: "Into The Badlands",
+      author: "Nanjira",
     },
     {
       id: 2,
       name: "Seal Six",
+      author: "Nanjira",
     },
     {
       id: 3,
       name: "The Last Ship",
+      author: "Nanjira",
     },
     {
       id: 4,
       name: "The Last Resort",
+      author: "Nanjira",
     },
   ];
   return (
@@ -23,13 +27,16 @@ function Container() {
         return (
           <div
             key={serie.id}
-            className={`flex p-2 rounded-md cursor-pointer transition ease-out delay-75 hover:scale-125 justify-center items-center max-w-max bg-gradient-to-br ${
+            className={`flex flex-col p-2 rounded-md cursor-pointer transition ease-out delay-75 hover:scale-125 justify-center items-center max-w-max bg-gradient-to-br ${
               serie.id % 2 === 0
                 ? "from-[#202221] to-[#283A34]"
                 : "from-[#22211F] to-[#442E29]"
             }`}
           >
-            <p className="font-thin">{serie.name}</p>
+            <p className="font-extralight">{serie.name}</p>
+            <span className="self-end font-thin text-xs italic">
+              {serie.author}
+            </span>
           </div>
         );
       })}
