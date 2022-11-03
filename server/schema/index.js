4a27e620fb8d6439a4b4ecda5fb2d0ad6ea8fafc
Abdul <abdul.nanjira@gmail.com>
2022-11-03 12:38:49 +0300

    making args required

--- server/schema/index.js
+++ server/schema/index.js
@@ -9,23 +9,8 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql;
-
-// dummy data
-// const books = [
-//   { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
-//   { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
-//   { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
-//   { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
-//   { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
-//   { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" },
-// ];
-
-// const authors = [
-//   { name: "Patrick Rothfuss", age: 44, id: "1" },
-//   { name: "Brandon Sanderson", age: 42, id: "2" },
-//   { name: "Terry Pratchett", age: 66, id: "3" },
-// ];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -95,8 +80,8 @@ const Mutations = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
       },
       resolve(parent, args) {
         let author = new Author({
@@ -109,9 +94,9 @@ const Mutations = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID },
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
         let book = new Book({
@@ -120,6 +105,13 @@ const Mutations = new GraphQLObjectType({
           authorId: args.authorId,
         });
         return book.save();
+      },
+    },
+    deleteBook: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(_, args) {
+        return Book.findByIdAndDelete(args.id);
       },
     },
   },
